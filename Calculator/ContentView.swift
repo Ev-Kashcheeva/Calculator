f17097a817d9ed3d7742621d0a5fd814ffadad4a
EvgeniyaKashcheeva <ev.kashcheeva@mail.ru>
2020-12-06 16:50:48 +0700

    Added functionality to the calculator

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var firstNumb = "0"
+    @State var secondNumb = "0"
+    @State var action = ""
     var body: some View {
         ZStack {
             Color("LowPan")
             VStack (spacing: 10) {
-                Text("0")
+                Text(action == "" ? firstNumb : secondNumb)
                     .foregroundColor(Color.white)
                     .padding(.trailing)
                     .font(.custom("SF Pro", size: 45))
@@ -49,7 +52,18 @@ struct ContentView: View {
                 Spacer()
                 HStack (spacing: 10) {
                     Button(action: {
-                        
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("7")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("7")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "7"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "7"
+                        }
                     }, label: {
                         Text("7")
                             .foregroundColor(Color.white)
@@ -60,7 +74,19 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("8")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("8")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "8"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "8"
+                        }
+
                     }, label: {
                         Text("8")
                             .foregroundColor(Color.white)
@@ -71,7 +97,18 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("9")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("9")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "9"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "9"
+                        }
                     }, label: {
                         Text("9")
                             .foregroundColor(Color.white)
@@ -83,7 +120,7 @@ struct ContentView: View {
                     
         
                     Button(action: {
-                        
+                        self.action = "multiply"
                     }, label: {
                         Text("*")
                             .foregroundColor(Color.black)
@@ -95,7 +132,18 @@ struct ContentView: View {
                 }
                 HStack (spacing: 10) {
                     Button(action: {
-                        
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("4")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("4")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "4"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "4"
+                        }
                     }, label: {
                         Text("4")
                             .foregroundColor(Color.white)
@@ -106,7 +154,18 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("5")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("5")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "5"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "5"
+                        }
                     }, label: {
                         Text("5")
                             .foregroundColor(Color.white)
@@ -117,7 +176,18 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("6")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("6")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "6"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "6"
+                        }
                     }, label: {
                         Text("6")
                             .foregroundColor(Color.white)
@@ -128,7 +198,8 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        self.action = "minus"
+
                     }, label: {
                         Text("-")
                             .foregroundColor(Color.black)
@@ -140,7 +211,18 @@ struct ContentView: View {
                 }
                 HStack (spacing: 10) {
                     Button(action: {
-                    
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("1")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("1")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "1"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "1"
+                        }
                     }, label: {
                         Text("1")
                             .foregroundColor(Color.white)
@@ -151,7 +233,18 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("2")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("2")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "2"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "2"
+                        }
                     }, label: {
                         Text("2")
                             .foregroundColor(Color.white)
@@ -162,6 +255,18 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("3")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("3")
+                        }
+                        if action == "" && firstNumb == "0" {
+                            self.firstNumb = "3"
+                        }
+                        if action != "" && secondNumb == "0"{
+                            self.secondNumb = "3"
+                        }
                         
                     }, label: {
                         Text("3")
@@ -173,7 +278,7 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        self.action = "plus"
                     }, label: {
                         Text("+")
                             .foregroundColor(Color.black)
@@ -185,7 +290,10 @@ struct ContentView: View {
                 }
                 HStack (spacing: 10) {
                     Button(action: {
-                        
+                        self.firstNumb = "0"
+                        self.secondNumb = "0"
+                        self.action = ""
+
                     }, label: {
                         Text("AC")
                             .foregroundColor(Color.white)
@@ -196,7 +304,12 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        if action == "" && firstNumb != "0" {
+                            self.firstNumb.append("0")
+                        }
+                        if action != "" && secondNumb != "0"{
+                            self.secondNumb.append("0")
+                        }
                     }, label: {
                         Text("0")
                             .foregroundColor(Color.white)
@@ -207,6 +320,25 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
+                        switch action {
+                        case "multiply":
+                            let res = Int(firstNumb)! * Int(secondNumb)!
+                            self.action = ""
+                            self.firstNumb = String(res)
+                        case "minus":
+                            let res = Int(firstNumb)! - Int(secondNumb)!
+                            self.action = ""
+                            self.firstNumb = String(res)
+                        case "plus":
+                            let res = Int(firstNumb)! + Int(secondNumb)!
+                            self.action = ""
+                            self.firstNumb = String(res)
+                        case "divide":
+                            let res = Int(firstNumb)! / Int(secondNumb)!
+                            self.action = ""
+                            self.firstNumb = String(res)
+                        default: break
+                        }
                         
                     }, label: {
                         Text("=")
@@ -218,7 +350,7 @@ struct ContentView: View {
                     .clipShape(Circle())
                     
                     Button(action: {
-                        
+                        self.action = "divide"
                     }, label: {
                         Text("/")
                             .foregroundColor(Color.black)
